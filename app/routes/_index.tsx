58f9db94fc767cdd09599e81577952576d92e2ba
Max López <maxlopez@max.local>
2023-05-13 15:02:05 -0600

    added image for url

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,6 +6,7 @@ export const meta: V2_MetaFunction = () => {
     { title: "Max López - UX Engineer" },
     { name: "description", content: "I am a UX Engineer passionate about creating user-friendly and intuitive experiences." },
     { name: "keywords", content: "UX/UI Designer, Front-end Developer, User Experience, User Interface, Web Design, Front-end Development" },
+    { property: "og:image", content: "https://i.ibb.co/sb66ykM/Bookmark.png" },
   ];
 };
 
